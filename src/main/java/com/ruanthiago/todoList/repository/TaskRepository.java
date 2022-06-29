71f1c8e7babd0c07a5c632f7f3ed4bc35ee69427
Ruan thiago <ruanthiago.in@gmail.com>
2022-06-28 23:20:30 -0300

    create, read e readById

--- src/main/java/com/ruanthiago/todoList/repository/TaskRepository.java
+++ src/main/java/com/ruanthiago/todoList/repository/TaskRepository.java
@@ -1,4 +1,10 @@
 package com.ruanthiago.todoList.repository;
 
-public interface TaskRepository {
+
+import com.ruanthiago.todoList.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
 }
